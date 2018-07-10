a484d75ebaa9ef562c4626ad777a4da730b6f47a
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-07-10 11:57:46 +0530

    Added gradient text and social media icons

--- src/Component/AboutComp.js
+++ src/Component/AboutComp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Grid, withWidth } from '@material-ui/core';
+import { withStyles, Grid, withWidth, SvgIcon } from '@material-ui/core';
 import { compose } from 'recompose';
 
 const styles = theme => ({
@@ -23,6 +23,12 @@ const styles = theme => ({
       right: '30%',
     }
   },
+  socialMediaGrid: {
+    bottom: 0,
+    position: 'absolute',
+    maxWidth: '720px',
+    margin: 'auto',
+  },
   media: {
     width: '100%',
     borderRadius: '50%',
@@ -41,22 +47,49 @@ const styles = theme => ({
     [theme.breakpoints.up('md')]: {
       fontSize: '2rem',
     },
-    color: '#fff',
+    background: 'linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundClip: 'text',
+    textFillColor: 'transparent',
     padding: '4px',
   }
 });
 
 const AboutComp = (props) => {
-  const { classes, width } = props;
+  const { classes } = props;
 
   return (
     <div className={classes.root}>
       <Grid className={classes.mainGrid} alignItems={'center'} container spacing={8}>
         <Grid item md={3} xs={12}>
-          <img className={classes.media} src='/images/profile.jpeg' />
+          <img alt='profileImage' className={classes.media} src='/images/profile.jpeg' />
         </Grid>
         <Grid item md={9} xs={12}>
           <p className={classes.contentStyle}>This is a Short Introduction about me. I currently study Computer Science and i love to code.</p>
+        </Grid>
+      </Grid>
+
+      <Grid className={classes.socialMediaGrid} alignItems={'center'} spacing={32} container>
+        <Grid item>
+          <SvgIcon style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
+            <path fill="#fff" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
+          </SvgIcon>
+        </Grid>
+        <Grid item>
+          <SvgIcon style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
+            <path fill="#fff" d="M17.71,9.33C18.19,8.93 18.75,8.45 19,7.92C18.59,8.13 18.1,8.26 17.56,8.33C18.06,7.97 18.47,7.5 18.68,6.86C18.16,7.14 17.63,7.38 16.97,7.5C15.42,5.63 11.71,7.15 12.37,9.95C9.76,9.79 8.17,8.61 6.85,7.16C6.1,8.38 6.75,10.23 7.64,10.74C7.18,10.71 6.83,10.57 6.5,10.41C6.54,11.95 7.39,12.69 8.58,13.09C8.22,13.16 7.82,13.18 7.44,13.12C7.81,14.19 8.58,14.86 9.9,15C9,15.76 7.34,16.29 6,16.08C7.15,16.81 8.46,17.39 10.28,17.31C14.69,17.11 17.64,13.95 17.71,9.33M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2Z" />
+          </SvgIcon>
+        </Grid>
+        <Grid item>
+          <SvgIcon style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
+            <path fill="#fff" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
+          </SvgIcon>
+        </Grid>
+        <Grid item>
+          <SvgIcon style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
+            <path fill="#fff" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z" />
+          </SvgIcon>
         </Grid>
       </Grid>
     </div>
