384eea5f657171de4378188b06872e8285cd40d3
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-11-05 22:20:50 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { lazy, Suspense, Component } from 'react';
 import NavBar from './Component/NavBar';
 import Footer from './Component/footerComp';
 import AboutComp from './Component/AboutComp';
-import ProjectComponent from './Component/ProjectsComp';
+// import ProjectComponent from './Component/ProjectsComp';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/store';
+
+const ProjectComponent = lazy(() => import('./Component/ProjectsComp'));
+
 const store = ConfigureStore();
 
 class App extends Component {
@@ -14,7 +17,9 @@ class App extends Component {
         <div>
           <NavBar />
           <AboutComp />
-          <ProjectComponent />
+          <Suspense fallback={<div>Fetching Project details... </div>}>
+            <ProjectComponent />
+          </Suspense>
           <Footer />
         </div>
       </Provider>
