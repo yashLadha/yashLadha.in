3e201cd67be0f18d85c7e965c92e617dbe27de73
Yash Ladha <yashladhapankajladha123@gmail.com>
2020-05-17 17:48:46 +0530

    added robots file for indexing by crawlers

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-robots-txt',
     'gatsby-plugin-styled-components',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {useStaticQuery, graphql} from 'gatsby';
-import {Container, Card} from 'components/common';
+import { useStaticQuery, graphql } from 'gatsby';
+import { Container, Card } from 'components/common';
 import starIcon from 'assets/icons/star.svg';
 import forkIcon from 'assets/icons/fork.svg';
-import {Wrapper, Grid, Item, Content, Stats} from './styles';
+import { Wrapper, Grid, Item, Content, Stats } from './styles';
 
 export const Projects = () => {
   const {
     github: {
       viewer: {
-        repositories: {edges},
+        repositories: { edges },
       },
     },
   } = useStaticQuery(
@@ -17,10 +17,7 @@ export const Projects = () => {
       {
         github {
           viewer {
-            repositories(
-              first: 8
-              orderBy: {field: STARGAZERS, direction: DESC}
-            ) {
+            repositories(first: 8, orderBy: { field: STARGAZERS, direction: DESC }) {
               edges {
                 node {
                   id
@@ -37,19 +34,14 @@ export const Projects = () => {
           }
         }
       }
-    `,
+    `
   );
   return (
     <Wrapper as={Container} id="projects">
       <h2>Projects</h2>
       <Grid>
-        {edges.map(({node}) => (
-          <Item
-            key={node.id}
-            as="a"
-            href={node.url}
-            target="_blank"
-            rel="noopener noreferrer">
+        {edges.map(({ node }) => (
+          <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer">
             <Card>
               <Content>
                 <h4>{node.name}</h4>
