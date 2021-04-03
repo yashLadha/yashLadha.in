ca15521dd0fad30302fc3700e0c669932a07858c
Yash Ladha <yashladhapankajladha123@gmail.com>
2021-04-03 10:55:10 +0530

    fix: center align find-more button
    Center alignment of button in find more is broken and adding either
    margins or not aligning at all. Used flex to place the items in center.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,13 @@ import ProjectsList from "./repoList";
 
 const projectCard = (project) => {
   return (
-    <a className="bg-gray-50" href={project.htmlURL} target="_blank" rel="noreferrer nofollow">
-      <div className="transition-shadow ease-in-out duration-500 h-full rounded overflow-hidden shadow-md hover:shadow-xl mb-4">
+    <a
+      className="bg-gray-50"
+      href={project.htmlURL}
+      target="_blank"
+      rel="noreferrer nofollow"
+    >
+      <div className="rounded-lg transition-shadow ease-in-out duration-500 h-full rounded overflow-hidden shadow-md hover:shadow-xl mb-4">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2 inline-flex">
             {project.name}
@@ -32,11 +37,17 @@ function Projects() {
       <div className="grid p-3 md:p-0 md:grid-cols-2 lg:grid-cols-3 grid-flow-row auto-rows-max md:grid-cols-2 gap-3">
         {ProjectsList.map(projectCard)}
       </div>
-      <button className="transition duration-300 ease-out hover:shadow-xl transform hover:-translate-y-0.5 hover:scale-105 bg-blue-400 p-3 text-white shadow-lg relative left-80 rounded-3xl">
-        <a href="https://github.com/yashLadha" target="_blank" rel="noreferrer">
-          Find More...
-        </a>
-      </button>
+      <div className="flex place-content-center mt-5">
+        <button className="transition duration-300 ease-out hover:shadow-xl transform hover:-translate-y-0.5 hover:scale-105 bg-blue-400 p-3 text-white shadow-lg rounded-3xl">
+          <a
+            href="https://github.com/yashLadha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Find More...
+          </a>
+        </button>
+      </div>
     </>
   );
 }
