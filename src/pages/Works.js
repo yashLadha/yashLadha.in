2176405d58057beb2ebd2e117ba68c1708f34f1d
Yash Ladha <yashladhapankajladha123@gmail.com>
2021-04-03 16:20:43 +0530

    add: style to mailto link

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -3,7 +3,7 @@ function Works() {
     <div>
       In case you are resonating with the work that i did and have a cool and
       kickass work that i can be a part of you can mail me at:
-      yash@yashladha.in.
+      <a className="text-blue-500" href="mailto:yash@yashladha.in">yash@yashladha.in.</a>
       <br /> <br />
     </div>
   );
