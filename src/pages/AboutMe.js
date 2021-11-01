3da4d2e850c0a5a817569ff48eabde57499aeba2
Yash Ladha <ladhay@amazon.com>
2021-11-01 15:59:04 +0530

    chore: update about me to reflect current job
    Updated the about me section to reflect the current company for which i
    am working. Replaced BrowserStack with amazon.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,15 +2,24 @@ function AboutMe() {
   return (
     <div className="px-2 md:px-0 font-regular leading-relaxed">
       <p className="mb-4 md:relative">
-        Hi there 👋. I am a Software Engineer currently working with
-        BrowserStack 🚀. Along with working on my day job i am also part of the
-        team that maintains the NodeJS core project and like to contribute to
-        other open source projects that equips others to solve problems.
+        Hi there 👋. I am a Software Engineer currently working with Amazon 🚀,
+        previously was part of the rocket ship BrowserStack. Along with working
+        on my day job i am also part of the team that maintains the NodeJS core
+        project and like to contribute to other open source projects that equips
+        others to solve problems.
       </p>
       <p className="mb-4 md:relative">
-        You can check out my <a className="text-indigo-600" href="/projects">projects</a> and other open
-        source work on Github{" "}
-        <a className="text-indigo-600" href="https://github.com/yashLadha" target="_blank" rel="noreferrer">
+        You can check out my{' '}
+        <a className="text-indigo-600" href="/projects">
+          projects
+        </a>{' '}
+        and other open source work on Github{' '}
+        <a
+          className="text-indigo-600"
+          href="https://github.com/yashLadha"
+          target="_blank"
+          rel="noreferrer"
+        >
           @yashLadha
         </a>
         .
