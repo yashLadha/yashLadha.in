062f153fc58a2cfdf56224ac0ea6289a77ffd88e
Yash Ladha <admin@yashladha.in>
2022-01-29 17:23:29 +0530

    fix: link for amazon on homepage

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -5,7 +5,7 @@ function AboutMe() {
     <div className="px-2 md:px-0 font-regular leading-relaxed">
       <p className="mb-4 md:relative">
         Hi there 👋. I am a Software Engineer currently working with{" "}
-        <StyledLink text="Amazon 🚀" href="/" />, previously was part of the
+        <StyledLink text="Amazon 🚀" href="https://amazon.com" />, previously was part of the
         rocket ship{" "}
         <StyledLink href="https://www.browserstack.com/" text="BrowserStack" />.
         Along with working on my day job, I am also part of the team that
