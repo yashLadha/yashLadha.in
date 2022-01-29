cf924c41cd2465f55fa59dd75c10cda62c36fc19
Yash Ladha <admin@yashladha.in>
2022-01-29 17:17:40 +0530

    refactor: change links to common widget
    Move links on home page to common widget so that all other can have
    consistent styling across all the pages.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,27 +1,23 @@
+import StyledLink from "./widgets/Link";
+
 function AboutMe() {
   return (
     <div className="px-2 md:px-0 font-regular leading-relaxed">
       <p className="mb-4 md:relative">
-        Hi there 👋. I am a Software Engineer currently working with Amazon 🚀,
-        previously was part of the rocket ship <a className="text-sky-800" href="https://www.browserstack.com/" target="_blank" rel="noreferrer">BrowserStack</a>. Along with working
-        on my day job, I am also part of the team that maintains the <a className="text-sky-800" href="https://github.com/nodejs/node" target="_blank" rel="noreferrer">NodeJS</a> core
+        Hi there 👋. I am a Software Engineer currently working with{" "}
+        <StyledLink text="Amazon 🚀" href="/" />, previously was part of the
+        rocket ship{" "}
+        <StyledLink href="https://www.browserstack.com/" text="BrowserStack" />.
+        Along with working on my day job, I am also part of the team that
+        maintains the{" "}
+        <StyledLink href="https://github.com/nodejs/node" text="NodeJS" /> core
         project.
       </p>
       <p className="mb-4 md:relative">
-        You can check out my{' '}
-        <a className="text-indigo-600" href="/projects">
-          projects
-        </a>{' '}
-        and other open source work on Github{' '}
-        <a
-          className="text-indigo-600"
-          href="https://github.com/yashLadha"
-          target="_blank"
-          rel="noreferrer"
-        >
-          @yashLadha
-        </a>
-        .
+        You can check out my{" "}
+        <StyledLink isBlank={false} href="/projects" text="projects" /> and
+        other open source work on Github{" "}
+        <StyledLink href="https://github.com/yashLadha" text="@yashLadha" />.
       </p>
     </div>
   );
