f6afe1b86ac90f7406ed53bc72dec758d271afeb
Yash Ladha <admin@yashladha.in>
2022-02-26 13:33:46 +0530

    feat: code splitting in content
    Enabled code splitting in react router to use individual components in
    rendering.

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import AboutMe from "./pages/AboutMe";
-import Projects from "./pages/Projects";
-import Works from "./pages/Works";
+import { lazy, Suspense } from 'react';
+
+const Projects = lazy(() => import("./pages/Projects"));
+const AboutMe = lazy(() => import("./pages/AboutMe"));
+const Works = lazy(() => import("./pages/Works"));
+const Loading = () => <div>Loading...</div>;
 
 function Content() {
   return (
     <div className="container rounded-md border-2 border-blue-700 border-opacity-50 mx-auto p-4 lg:w-1/2 mt-2 py-6">
-      <Routes>
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/works" element={<Works />} />
-        <Route path="/" element={<AboutMe />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/" element={<AboutMe />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
