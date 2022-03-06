5d387a829e42b0926937a95376a8224b2e10fadb
Yash Ladha <yash@yashladha.in>
2022-03-06 08:24:42 +0530

    chore: rephrase contact-me section

--- src/pages/contactme.js
+++ src/pages/contactme.js
@@ -5,8 +5,8 @@ function Works() {
   return (
     <Layout>
       <div>
-        In case you are resonating with the work that i did and have a cool and
-        kickass work that I can be a part of you can mail me at:
+        In case you are resonating with the work that I did and have a cool and
+        kickass work that I can be a part of you can reach out to via email @:
         <a className="text-blue-500" href="mailto:yash@yashladha.in">yash@yashladha.in.</a>
         <br /> <br />
       </div>
