ef0bb0a9ab04726c001ad7665513b1aead415268
Yash Ladha <yash@yashladha.in>
2022-07-24 06:58:19 +0530

    chore: minor content fixes for page

--- src/pages/contactme.js
+++ src/pages/contactme.js
@@ -5,9 +5,12 @@ function Works() {
   return (
     <Layout>
       <div>
-        In case you are resonating with the work that I did and have a cool and
-        kickass work that I can be a part of you can reach out to via email @:
-        <a className="text-blue-500" href="mailto:yash@yashladha.in">yash@yashladha.in.</a>
+        In case you are resonating with the work that I did and have some
+        interested projects that I can be a part of, you can reach out to via
+        email @:
+        <a className="text-blue-500" href="mailto:yash@yashladha.in">
+          yash@yashladha.in.
+        </a>
         <br /> <br />
       </div>
     </Layout>
