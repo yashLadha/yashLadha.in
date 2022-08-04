c0eab0250b6e994089a9b88ca60405f9e179c77e
Yash Ladha <yash@yashladha.in>
2022-08-04 07:13:19 +0530

    reformat: gatsby config changes
    Reformatted config changes for gatsby and updated description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,39 @@
 module.exports = {
   siteMetadata: {
     title: `Yash Ladha`,
-    url: 'https://yashladha.in',
-    titleTemplate: '%s',
-    image: '/icon.png',
+    url: "https://yashladha.in",
+    titleTemplate: "%s",
+    image: "/icon.png",
     siteUrl: `https://yashladha.in`,
-    description: `Hi there, I am a Software Engineer and this is my personal website.`,
-    twitterUsername: `yashLadha_`
+    description: `Hi there, I am a Software Engineer in day and open source developer in night.`,
+    twitterUsername: `yashLadha_`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-postcss", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      icon: "src/images/icon.png",
-      name: `Yash Ladha`,
-      short_name: `yashLadha`,
-      start_url: `/`,
-      theme_color: "#000000",
-      background_color: "#ffffff",
-      display: `standalone`
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-      resolve: 'gatsby-source-filesystem',
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
       options: {
-        "name": "images",
-        "path": "./src/images/"
+        icon: "src/images/icon.png",
+        name: `Yash Ladha`,
+        short_name: `yashLadha`,
+        start_url: `/`,
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        display: `standalone`,
       },
-      __key: "images"
-    }]
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
