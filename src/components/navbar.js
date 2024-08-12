e3a80ee1e3b53323b0bc28b6238f4a4c2a548f5f
Yash Ladha <yash@yashladha.in>
2024-08-12 17:42:01 +0530

    chore: update blog link on index page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function NavBarComponent({children}) {
   return (
-    <div className="capitalize transition duration-300 ease-in-out hover:text-blue-500 hover:font-bold hover:rounded-lg hover:shadow-lg text-blue-700 p-2 m-0 md:m-2">
+    <div className="capitalize transition duration-300 ease-in-out hover:text-blue-500 hover:font-bold text-blue-700 p-2 m-0 md:m-2">
       {children}
     </div>
   )
@@ -18,7 +18,7 @@ function NavBar() {
         <a href="/projects">Projects</a>
       </NavBarComponent>
       <NavBarComponent>
-        <a href="https://blog.yashladha.in/blog" target="_blank" rel="noreferrer">Blog</a>
+        <a href="https://blog.yashladha.in" target="_blank" rel="noreferrer">Blog</a>
       </NavBarComponent>
       <NavBarComponent>
         <a href="/contactme">Contact Me</a>
