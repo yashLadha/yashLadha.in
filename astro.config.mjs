b880fa5150659d186310937d56ac3b7598bfd40a
Yash Ladha <yash@yashladha.in>
2025-01-13 07:32:43 +0530

    chore: update blog post theming for code snippets
    Update code snippet theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,14 @@ export default defineConfig({
   site: "https://yashladha.in",
   prefetch: true,
   trailingSlash: "never",
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'tokyo-night',
+      },
+    },
+  },
   build: {
     format: "file", // Fix trailing slash never in production builds
   },
